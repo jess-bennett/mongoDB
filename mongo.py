70d4126a514fee026afb078dbb1708bbc41a9d70
jess-bennett <jess.bennett@posteo.net>
2020-09-27 11:15:37 +0000

    End of mongo project

---
+++ mongo.py
@@ -0,0 +1,31 @@
+import os
+import pymongo
+if os.path.exists("env.py"):
+    import env
+
+
+MONGODB_URI = os.getenv("MONGO_URI")
+DBS_NAME = "myTestDB"
+COLLECTION_NAME = "myFirstMDB"
+
+
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        print("Mongo is connected!")
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("Could not connect to MongoDB: %s") % e
+
+
+conn = mongo_connect(MONGODB_URI)
+
+coll = conn[DBS_NAME][COLLECTION_NAME]
+
+coll.update_many({'nationality': 'american'}, {'$set': {'hair_colour':
+                 'maroon'}})
+
+documents = coll.find()
+
+for doc in documents:
+    print(doc)
